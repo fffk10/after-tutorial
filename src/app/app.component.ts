1184e333d3937894358bd74807e4c5d98d6fdd6f
fffk10 <warafffk10@gmail.com>
2021-01-30 23:51:34 +0900

    make project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'after-tutorial';
+
+  users: User[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<{ data: User[] }>('https://reqres.in/api/users').subscribe(resp => {
+      this.users = resp.data;
+    });
+  }
 }
